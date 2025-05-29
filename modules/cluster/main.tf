540b93af61656c6ce67cc65a195dcf1078ca2f64
pbarysiuk <sh.holod@gmail.com>
2025-05-29 19:30:37 +0200

    fix: port mapping for http&https

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -28,16 +28,19 @@ resource "kind_cluster" "default" {
       ]
 
       extra_port_mappings {
-        container_port = 80
-        host_port      = 30080
+        container_port = 30080
+        host_port      = 80
+        protocol       = "TCP"
       }
       extra_port_mappings {
-        container_port = 443
-        host_port      = 30443
+        container_port = 30443
+        host_port      = 443
+        protocol       = "TCP"
       }
       extra_port_mappings {
         container_port = 6443
         host_port      = 30444
+        protocol       = "TCP"
       }
     }
 
