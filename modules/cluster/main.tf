304993db4fde34378b81c42b475ccaf5035549c7
pbarysiuk <sh.holod@gmail.com>
2025-06-10 16:17:02 +0200

    fix: yaml move to sep folder

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -22,7 +22,7 @@ resource "kind_cluster" "default" {
       role = "control-plane"
 
       kubeadm_config_patches = [
-        templatefile("${path.module}/kubeadm_patch.yaml.tpl", {
+        templatefile("${path.module}/yaml/kubeadm_patch.yaml.tpl", {
           domain = var.domain
         })
       ]
@@ -58,9 +58,20 @@ resource "null_resource" "install_calico" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/operator-crds.yaml
-      kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/tigera-operator.yaml
+      kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/operator-crds.yaml
+      kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/tigera-operator.yaml
       kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/custom-resources.yaml
+      kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${var.calico_ver}/manifests/calico.yaml
     EOT
   }
 }
+
+# data "kubectl_path_documents" "docs" {
+#     pattern = "${path.module}/yaml/calico*.yaml"
+# }
+
+# resource "kubectl_manifest" "test" {
+#     for_each  = toset(data.kubectl_path_documents.docs.documents)
+#     yaml_body = each.value
+#     depends_on = [resource.kind_cluster.default]
+# }
